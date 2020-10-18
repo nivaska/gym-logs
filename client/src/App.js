0e821d563b7a194d6d32df5f9c1bc6ebdfe54678
nivas ka <nivas619ka@gmail.com>
2020-10-18 16:52:58 +0200

    created sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,32 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route, Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import "./index.css";
 import HomePage from "./pages/Home";
 import FindPage from "./pages/Find";
 import ManagePage from "./pages/Manage";
+import SideBar from "./components/SideBar";
 
 export default () => {
   return (
     <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/find">Find</Link>
-          </li>
-          <li>
-            <Link to="/manage">Manage</Link>
-          </li>
-        </ul>
+      <nav className="side-nav">
+        <SideBar />
       </nav>
 
-  
-      <Switch>
-        <Route path="/find">
-          <FindPage />
-        </Route>
-        <Route path="/manage">
-          <ManagePage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+      <main className="main-conent">
+        <Switch>
+          <Route path="/find">
+            <FindPage />
+          </Route>
+          <Route path="/manage">
+            <ManagePage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 };
--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,9 +1,78 @@
-import React, { Component } from 'react';   
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-class SideBar extends Component{
-    render(){
-        return "Find"
-    }
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import HomeIcon from "@material-ui/icons/Home";
+import TodayIcon from "@material-ui/icons/Today";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import LanguageIcon from "@material-ui/icons/Language";
+
+const styles = (theme) => ({
+  navButton: {
+    textTransform: "none",
+    color: "green",
+  },
+});
+
+class SideBar extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <ul>
+        <li>
+          <Button
+            size="large"
+            color="default"
+            className={classes.navButton}
+            startIcon={<HomeIcon />}
+            href="/"
+          />
+        </li>
+        <li>
+          <Button
+            size="large"
+            color="default"
+            className={classes.navButton}
+            startIcon={<TodayIcon />}
+            href="/find"
+          />
+        </li>
+
+        <li>
+          <Button
+            size="large"
+            color="default"
+            className={classes.navButton}
+            startIcon={<SettingsIcon />}
+            href="/manage"
+          />
+        </li>
+        <li>
+          <Button
+            disabled
+            size="large"
+            color="default"
+            className={classes.navButton}
+            startIcon={<AssessmentIcon />}
+            href="/analyse"
+          />
+        </li>
+        <li>
+          <Button
+            size="large"
+            disabled
+            color="default"
+            className={classes.navButton}
+            startIcon={<LanguageIcon />}
+            href="/links"
+          />
+        </li>
+      </ul>
+    );
+  }
 }
 
-export default FindPage;
+export default withStyles(styles)(SideBar);
